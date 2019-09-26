5dc330b75f0094761584b7018725cf8189dfb091
thitipat <thitipat.cpe18@hotmail.com>
2019-09-26 19:14:57 +0700

    insert price and package test page.

--- cypress/integration/goonvpn_work_flow/2_resister_work_flow.spec.js
+++ cypress/integration/goonvpn_work_flow/2_resister_work_flow.spec.js
@@ -12,143 +12,143 @@ describe("Work flow : Register", () => {
     cy.url().should("include", "signup");
   });
   it("Register : Failed, User empty case ", () => {
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .type(email, { delay: 20 });
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .type(pass, { delay: 20 });
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .click();
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]').should("contain", "กรุณากรอกชื่อผู้ใช้งาน");
-  });
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .type(email, { delay: 20 });
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .type(pass, { delay: 20 });
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .click();
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]').should("contain", "กรุณากรอกชื่อผู้ใช้งาน");
+  // });
 
-  it("Register : Failed, Email empty case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .type(user, { delay: 20 });
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .type(pass, { delay: 20 });
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .click();
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]').should("contain", "กรุณากรอกอีเมลล์ของท่าน");
-  });
+  // it("Register : Failed, Email empty case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .type(user, { delay: 20 });
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .type(pass, { delay: 20 });
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .click();
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]').should("contain", "กรุณากรอกอีเมลล์ของท่าน");
+  // });
 
-  it("Register : Failed, Password empty case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .type(user, { delay: 20 });
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .type(email, { delay: 20 });
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .click();
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]').should(
-      "contain",
-      "กรุณากรอกรหัสผ่านของท่าน"
-    );
-  });
+  // it("Register : Failed, Password empty case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .type(user, { delay: 20 });
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .type(email, { delay: 20 });
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .click();
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]').should(
+  //     "contain",
+  //     "กรุณากรอกรหัสผ่านของท่าน"
+  //   );
+  // });
 
-  it("Register : Failed, User and Email empty case ", () => {
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .type(pass, { delay: 20 });
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .click();
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .click();
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
-      .and("contain", "กรุณากรอกอีเมลล์ของท่าน");
-  });
+  // it("Register : Failed, User and Email empty case ", () => {
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .type(pass, { delay: 20 });
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .click();
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .click();
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
+  //     .and("contain", "กรุณากรอกอีเมลล์ของท่าน");
+  // });
 
-  it("Register : Failed, User and Password empty case ", () => {
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .type(email, { delay: 20 });
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .click()
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .click()
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
-      .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
-  });
+  // it("Register : Failed, User and Password empty case ", () => {
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .type(email, { delay: 20 });
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .click()
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .click()
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
+  //     .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
+  // });
 
-  it("Register : Failed, Email and Password empty case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .type(user, { delay: 20 });
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .click()
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .click()
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "กรุณากรอกอีเมลล์ของท่าน")
-      .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
-  });
+  // it("Register : Failed, Email and Password empty case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .type(user, { delay: 20 });
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .click()
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .click()
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "กรุณากรอกอีเมลล์ของท่าน")
+  //     .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
+  // });
 
-  it("Register : Failed, User Email and Password empty case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .click()
-    cy.get("#signupform-email")
-      .should("have.id", "signupform-email")
-      .click()
-    cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .click()
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
-      .and("contain", "กรุณากรอกอีเมลล์ของท่าน")
-      .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
-  });
+  // it("Register : Failed, User Email and Password empty case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .click()
+  //   cy.get("#signupform-email")
+  //     .should("have.id", "signupform-email")
+  //     .click()
+  //   cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .click()
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "กรุณากรอกชื่อผู้ใช้งาน")
+  //     .and("contain", "กรุณากรอกอีเมลล์ของท่าน")
+  //     .and("contain", "กรุณากรอกรหัสผ่านของท่าน");
+  // });
 
-  it("Register : Failed, Email invalid case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .type(user)
-      cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .type(pass)
-      cy.get("#signupform-email")
-        .should("have.id", "signupform-email")
-        .type('aaa')
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "อีเมลล์ ไม่ถูกต้อง")
-  });
+  // it("Register : Failed, Email invalid case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .type(user)
+  //     cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .type(pass)
+  //     cy.get("#signupform-email")
+  //       .should("have.id", "signupform-email")
+  //       .type('aaa')
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "อีเมลล์ ไม่ถูกต้อง")
+  // });
 
-  it("Register : Failed, Password invalid case ", () => {
-    cy.get("#signupform-username")
-      .should("have.id", "signupform-username")
-      .type(user)
-      cy.get("#signupform-email")
-        .should("have.id", "signupform-email")
-        .type(email)
-      cy.get("#signupform-password")
-      .should("have.id", "signupform-password")
-      .type('pass')
-    cy.get(".field-signupform-email > .control-label").click();
-    cy.get('[class="help-block"]')
-      .should("contain", "รหัสผ่านต้องมากกว่า 6 ตัวอักษร")
+  // it("Register : Failed, Password invalid case ", () => {
+  //   cy.get("#signupform-username")
+  //     .should("have.id", "signupform-username")
+  //     .type(user)
+  //     cy.get("#signupform-email")
+  //       .should("have.id", "signupform-email")
+  //       .type(email)
+  //     cy.get("#signupform-password")
+  //     .should("have.id", "signupform-password")
+  //     .type('pass')
+  //   cy.get(".field-signupform-email > .control-label").click();
+  //   cy.get('[class="help-block"]')
+  //     .should("contain", "รหัสผ่านต้องมากกว่า 6 ตัวอักษร")
   });
   // it("Register : Complete", () => {
   //   cy.get("#signupform-username")
--- cypress/integration/goonvpn_work_flow/3_login_work_flow.spec.js
+++ cypress/integration/goonvpn_work_flow/3_login_work_flow.spec.js
@@ -13,61 +13,61 @@ describe("Work flow : Login", () => {
   });
 
   it("Login : Failed,User field empty case", () => {
-    cy.get("#loginform-password")
-      .should("have.id", "loginform-password")
-      .type(pass, { delay: 20 });
-    cy.get("#loginform-username")
-      .should("have.id", "loginform-username")
-      .click();
-    cy.get(":nth-child(5) > .container > .row").click();
-    cy.get('[class="col-sm-offset-4 col-sm-8"]')
-      .children()
-      .should("have.class", "help-block")
-      .and("contain", "กรุณากรอก ชื่อผู้ใช้ หรือ อีเมลล์ ");
-  });
-  it("Login : Failed,Password field empty case", () => {
-    cy.visit(urlLogin);
-    cy.get("#loginform-username")
-      .should("have.id", "loginform-username")
-      .type(email, { delay: 20 });
-    cy.get("#loginform-password")
-      .should("have.id", "loginform-password")
-      .click();
-    cy.get(":nth-child(5) > .container > .row").click();
-    cy.get('[class="col-sm-offset-4 col-sm-8"]')
-      .children()
-      .should("have.class", "help-block")
-      .and("contain", "กรุณากรอก รหัสผ่าน ");
-  });
-  it("Login : Failed,User and Password field empty case", () => {
-    cy.visit(urlLogin);
-    cy.get("#loginform-username")
-      .should("have.id", "loginform-username")
-      .click();
-    cy.get("#loginform-password")
-      .should("have.id", "loginform-password")
-      .click();
-    cy.get(":nth-child(5) > .container > .row").click();
-    cy.get('[class="col-sm-offset-4 col-sm-8"]')
-      .children()
-      .should("have.class", "help-block")
-      .and("contain", "กรุณากรอก ชื่อผู้ใช้ หรือ อีเมลล์ ")
-      .and("contain", "กรุณากรอก รหัสผ่าน ");
-  });
-  it("Login : Complete.", () => {
-    cy.get('[class="btn-topbar login"]').click();
-    cy.url().should("include", "login");
-    cy.get("#loginform-username")
-      .click()
-      .should("have.id", "loginform-username")
-      .type(email, { delay: 20 });
-    cy.get("#loginform-password")
-      .should("have.id", "loginform-password")
-      .type(pass, { delay: 20 });
-    cy.setCookie(
-      "_identity",
-      "14bd1e44832aa71ac1555d838164fce36e7ea3b812e9155d148c2ef8d0d1f3aca%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A50%3A%22%5B57856%2C%22ptrcRZnG37LiJnIATMxs5QV5O6CXCGE7%22%2C2592000%5D%22%3B%7D"
-    );
-    cy.visit(urlLoged);
+  //   cy.get("#loginform-password")
+  //     .should("have.id", "loginform-password")
+  //     .type(pass, { delay: 20 });
+  //   cy.get("#loginform-username")
+  //     .should("have.id", "loginform-username")
+  //     .click();
+  //   cy.get(":nth-child(5) > .container > .row").click();
+  //   cy.get('[class="col-sm-offset-4 col-sm-8"]')
+  //     .children()
+  //     .should("have.class", "help-block")
+  //     .and("contain", "กรุณากรอก ชื่อผู้ใช้ หรือ อีเมลล์ ");
+  // });
+  // it("Login : Failed,Password field empty case", () => {
+  //   cy.visit(urlLogin);
+  //   cy.get("#loginform-username")
+  //     .should("have.id", "loginform-username")
+  //     .type(email, { delay: 20 });
+  //   cy.get("#loginform-password")
+  //     .should("have.id", "loginform-password")
+  //     .click();
+  //   cy.get(":nth-child(5) > .container > .row").click();
+  //   cy.get('[class="col-sm-offset-4 col-sm-8"]')
+  //     .children()
+  //     .should("have.class", "help-block")
+  //     .and("contain", "กรุณากรอก รหัสผ่าน ");
+  // });
+  // it("Login : Failed,User and Password field empty case", () => {
+  //   cy.visit(urlLogin);
+  //   cy.get("#loginform-username")
+  //     .should("have.id", "loginform-username")
+  //     .click();
+  //   cy.get("#loginform-password")
+  //     .should("have.id", "loginform-password")
+  //     .click();
+  //   cy.get(":nth-child(5) > .container > .row").click();
+  //   cy.get('[class="col-sm-offset-4 col-sm-8"]')
+  //     .children()
+  //     .should("have.class", "help-block")
+  //     .and("contain", "กรุณากรอก ชื่อผู้ใช้ หรือ อีเมลล์ ")
+  //     .and("contain", "กรุณากรอก รหัสผ่าน ");
+  // });
+  // it("Login : Complete.", () => {
+  //   cy.get('[class="btn-topbar login"]').click();
+  //   cy.url().should("include", "login");
+  //   cy.get("#loginform-username")
+  //     .click()
+  //     .should("have.id", "loginform-username")
+  //     .type(email, { delay: 20 });
+  //   cy.get("#loginform-password")
+  //     .should("have.id", "loginform-password")
+  //     .type(pass, { delay: 20 });
+  //   cy.setCookie(
+  //     "_identity",
+  //     "14bd1e44832aa71ac1555d838164fce36e7ea3b812e9155d148c2ef8d0d1f3aca%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A50%3A%22%5B57856%2C%22ptrcRZnG37LiJnIATMxs5QV5O6CXCGE7%22%2C2592000%5D%22%3B%7D"
+  //   );
+  //   cy.visit(urlLoged);
   });
 });
