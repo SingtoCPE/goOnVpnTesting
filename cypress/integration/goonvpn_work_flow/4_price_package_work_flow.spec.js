1f097fe18d8cd875abbe2da01f221754dde089a1
thitipat <thitipat.cpe18@hotmail.com>
2019-09-29 21:26:51 +0700

    update project.

--- cypress/integration/goonvpn_work_flow/4_price_package_work_flow.spec.js
+++ cypress/integration/goonvpn_work_flow/4_price_package_work_flow.spec.js
@@ -5,52 +5,70 @@ describe("Work flow : Price and package", () => {
     cy.beforeEachPrice(); //commands
   });
 
-  it("Work flow: ดูเลขที่บัญชี > Topup > รับวันทดลองฟรี", () => {
-    cy.viewAccountNumber(); //commands
-    cy.get('[class="special btn-get-trial"]')
-      .contains(" รับวันทดลองใช้งานฟรี!")
-      .should("have.attr", "href", "/promotion/get-trial")
-      .and("have.text", " รับวันทดลองใช้งานฟรี!")
-      .click();
-    cy.url().should("include", "/get-trial");
-  });
+//   it("Work flow: ดูเลขที่บัญชี > Topup > รับวันทดลองฟรี", () => {
+//     cy.viewAccountNumber(); //commands
+//     cy.get('[class="special btn-get-trial"]')
+//       .contains(" รับวันทดลองใช้งานฟรี!")
+//       .should("have.attr", "href", "/promotion/get-trial")
+//       .and("have.text", " รับวันทดลองใช้งานฟรี!")
+//       .click();
+//     cy.url().should("include", "/get-trial");
+//   });
 
-  it("Work flow: ดูเลขที่บัญชี > Topup > เติมวันใช้งาน", () => {
-    cy.viewAccountNumber(); //commands
-    cy.loopClickTopupGP(); //commands
-    cy.loopClickCancel(); //commands
-  });
+//   it("Work flow: ดูเลขที่บัญชี > Topup > เติมวันใช้งาน", () => {
+//     cy.viewAccountNumber(); //commands
+//     cy.loopClickTopupGP(); //commands
+//     cy.loopClickCancel(); //commands
+//   });
 
-  it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ > เติม GP ด้วยบัตรทรูมันนี่​", () => {
-    cy.topupTruemoney(); //commands
-  });
-//ต้องเติมเงิน true money ถึงจะ test เสร็จ -----
-  it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ > Invalid เติม GP ด้วยบัตรทรูมันนี่​", () => {
-    cy.topupTruemoney(); //commands
-    cy.get('[class="text-center form-control"]')
-      .and("have.id", "tmn_password")
-      .type("1234");
-    cy.get('[class="hvr-grow-shadow btn btn-theme-bg"]')
-      .contains(" เติมเงิน")
-      .should("have.text", " เติมเงิน")
-      .click()
-      .get('[class="ui-dialog-title"]')
-      .contains("เกิดข้อผิดพลาด")
-      .should("have.text", "เกิดข้อผิดพลาด")
-      .get('[class="ui-button ui-corner-all ui-widget"]')
-      .contains("ปิด")
-      .should("have.text", "ปิด")
-      .click();
-  });
+//   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ > เติม GP ด้วยบัตรทรูมันนี่​", () => {
+//     cy.topupTruemoney(); //commands
+//   });
+// //ต้องเติมเงิน true money ถึงจะ test เสร็จ -----
+//   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ > Invalid เติม GP ด้วยบัตรทรูมันนี่​", () => {
+//     cy.topupTruemoney(); //commands
+//     cy.get('[class="text-center form-control"]')
+//       .and("have.id", "tmn_password")
+//       .type("1234");
+//     cy.get('[class="hvr-grow-shadow btn btn-theme-bg"]')
+//       .contains(" เติมเงิน")
+//       .should("have.text", " เติมเงิน")
+//       .click()
+//       .get('[class="ui-dialog-title"]')
+//       .contains("เกิดข้อผิดพลาด")
+//       .should("have.text", "เกิดข้อผิดพลาด")
+//       .get('[class="ui-button ui-corner-all ui-widget"]')
+//       .contains("ปิด")
+//       .should("have.text", "ปิด")
+//       .click();
+//   });
 
-  it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ >  เติม GP ด้วยบัตรทรูมันนี่", () => {
+//   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ >  เติม GP ด้วยบัตรทรูมันนี่", () => {
 
-  });
+//   });
+
+//   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ >  ชำระผ่าน PayPal > กรอก ข้อมูลการชำระเงินผิด", () => {
+//     cy.viewAccountNumber(); // commands
+//     cy.loopSelectPaypal(); // commands
+// // - ติดปัญหากดแล้วเด้งไปหน้าอื่น
+// // - ต้องลองจ่ายแบบ paypal
+//   });
+
+//   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ >  ชำระผ่าน PayPal > กรอก ข้อมูลการชำระเงินถูก", () => {
+//     cy.viewAccountNumber(); // commands
+//     cy.loopSelectPaypal(); // commands
+// // - ติดปัญหากดแล้วเด้งไปหน้าอื่น
+// // - ต้องลองจ่ายแบบ paypal
+//   });
 
   it("Work flow: ดูเลขที่บัญชี > ชำระค่าบริการ >  ชำระผ่าน PayPal > กรอก ข้อมูลการชำระเงินผิด", () => {
     cy.viewAccountNumber(); // commands
-    cy.loopSelectPaypal(); // commands
-// - ติดปัญหากดแล้วเด้งไปหน้าอื่น
-// - ต้องลองจ่ายแบบ paypal
+    cy.get('[class="sub-menu"]')
+      .contains(' แจ้งโอนเงิน')
+      .click()
+    cy.url().should("include", "/deposit-notify");
+    cy.get('[class="btn border-theme animated shake"]')
+      .contains(' บัญชีธนาคารที่รองรับ')
+      .click()
   });
 });
