c70eaae1bc81620124bf1611620372b9bcab4008
thitipat <thitipat.cpe18@hotmail.com>
2019-10-02 15:26:16 +0700

    Update project.

--- cypress/integration/goonvpn_work_flow/5_installation.spec.js
+++ cypress/integration/goonvpn_work_flow/5_installation.spec.js
@@ -40,20 +40,41 @@ describe("Work flow : Installation", () => {
   //   cy.url().should('include','/pages/update-softether#')
   // });
 
-  it("Work flow : Installation > โปรแกรม Softether VPN Client > โปรแกรม Tunnelblick สำหรับ Mac", () => {
+  // it("Work flow : Installation > โปรแกรม Softether VPN Client > โปรแกรม Tunnelblick สำหรับ Mac", () => {
+  //   cy.get('[class="dropdown-menu"]')
+  //     .contains("โปรแกรม Tunnelblick สำหรับ Mac")
+  //     .should('have.attr','href','/pages/install-tunnelblick')
+  //     .click();
+  //   cy.get('[class="sub-link"]')
+  //     .contains(' ดาวน์โหลด Tunnelblick')
+  //     .should('have.attr','href','/get-client')
+  //     .click()
+  //   cy.get('[class="btn btn-lg border-theme"]')
+  //     .contains(' ดูวิธีการใช้งาน Tunnelblick กันต่อเลย')
+  //     .should('have.attr','href','/pages/use-tunnelblick')
+  //     .click()
+  //   cy.url().should('include','/pages/use-tunnelblick')
+  // });
+
+  it("Work flow : Installation > โปรแกรม Softether VPN Client > OpenVPN Connect สำหรับ iPhone, iPad", () => {
     cy.get('[class="dropdown-menu"]')
-      .contains("โปรแกรม Tunnelblick สำหรับ Mac")
-      .should('have.attr','href','/pages/install-tunnelblick')
+      .contains("OpenVPN Connect สำหรับ iPhone, iPad")
+      .should("have.attr", "href", "/pages/install-openvpn-connect-ios")
       .click();
     cy.get('[class="sub-link"]')
-      .contains(' ดาวน์โหลด Tunnelblick')
-      .should('have.attr','href','/get-client')
-      .click()
-    cy.get('[class="btn btn-lg border-theme"]')
-      .contains(' ดูวิธีการใช้งาน Tunnelblick กันต่อเลย')
-      .should('have.attr','href','/pages/use-tunnelblick')
-      .click()
-    cy.url().should('include','/pages/use-tunnelblick')
-    
+      .contains(" ดาวน์โหลด")
+      .should("have.attr", "href", "/get-client")
+      .click();
+  });
+
+  it("Work flow : Installation > โปรแกรม Softether VPN Client > OpenVPN Connect สำหรับ Android", () => {
+    cy.get('[class="dropdown-menu"]')
+      .contains("OpenVPN Connect สำหรับ Android")
+      .should("have.attr", "href", "/pages/install-openvpn-connect-android")
+      .click();
+    cy.get('[class="sub-link"]')
+      .contains(" ดาวน์โหลด")
+      .should("have.attr", "href", "/get-client")
+      .click();
   });
 });
