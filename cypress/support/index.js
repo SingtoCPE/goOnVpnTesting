10221a0a092261972dc9e96e3643e12c94cd462b
thitipat <thitipat.cpe18@hotmail.com>
2019-10-02 10:36:09 +0700

    update project.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,7 +1,7 @@
 import './commands'
-import './mainpage_commands'
-import './register_commands'
-import './login_commands'
-import './price_package_commands'
+import './1_mainpage_commands'
+import './2_register_commands'
+import './3_login_commands'
+import './4_price_package_commands'
 
 
