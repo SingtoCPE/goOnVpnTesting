acbcaafb81791139f794f4cfe6ad77d0f71babcb
thitipat <thitipat.cpe18@hotmail.com>
2019-10-02 18:29:05 +0700

    Update project.

---
+++ cypress/integration/goonvpn_work_flow/7_download.spec.js
@@ -0,0 +1,50 @@
+/// <reference types = 'cypress'/>
+
+describe("Work flow : Download", () => {
+  beforeEach("Go to Download.", () => {
+    cy.beforeEachDowload(); //commands
+  });
+
+  it("Work flow : Download > download for window > Go Connect v1.5.0.0", async () => {
+    await cy
+      .get('[class="btn border-theme  btn-block"]')
+      .should("have.attr", "href", "/download/go-connect")
+      .contains("ดาวน์โหลด")
+      .click();
+    Cypress.config("pageLoadTimeout");
+  });
+
+  it("Work flow : Download > download for window > Softether VPN Client", async () => {
+    await cy
+      .get(":nth-child(2) > :nth-child(2) > .btn")
+      .contains(" ดาวน์โหลด")
+      .and("have.attr", "href", "/download/softether")
+      .click();
+  });
+
+  it("Work flow : Download > download for Mac OS X > Tunnelblick 3.7.5", async () => {
+    await cy
+      .get(
+        ":nth-child(2) > .text-center > .table-responsive > .table > tbody > tr > :nth-child(2) > .btn"
+      )
+      .contains(" ดาวน์โหลด")
+      .and("have.attr", "href", "/download/osx")
+      .click();
+  });
+
+  it("Work flow : Download > download for IOS and Androin > Itunes Store", async () => {
+    await cy
+      .get('[href="/download/ios"]')
+      .contains(" Itunes Store")
+      .and("have.attr", "href", "/download/ios")
+      .click();
+  });
+
+  it("Work flow : Download > download for IOS and Androin > Play Store", async () => {
+    await cy
+      .get('[href="/download/android"]')
+      .contains(" Play Store")
+      .and("have.attr", "href", "/download/android")
+      .click();
+  });
+});
---
+++ cypress/support/7_download_commands.js
@@ -0,0 +1,11 @@
+Cypress.Commands.add("beforeEachDowload", () => {
+  cy.setCookie(
+    "_identity",
+    "14bd1e44832aa71ac1555d838164fce36e7ea3b812e9155d148c2ef8d0d1f3aca%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A50%3A%22%5B57856%2C%22ptrcRZnG37LiJnIATMxs5QV5O6CXCGE7%22%2C2592000%5D%22%3B%7D"
+  );
+  cy.visit(Cypress.env("URLMAIN")).wait(500);
+  cy.url().should("include", "/");
+  cy.get('#w1 > :nth-child(5) > a')
+    .contains("ดาวน์โหลด")
+    .click();
+});
