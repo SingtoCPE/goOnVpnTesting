d146acc4ef9ebeb855ee5636864bd338da5ad702
thitipat <thitipat.cpe18@hotmail.com>
2019-10-03 14:28:01 +0700

    Update porject.

--- cypress/integration/goonvpn_work_flow/test.spec.js
+++ cypress/integration/goonvpn_work_flow/test.spec.js
@@ -3,12 +3,19 @@ describe("Work flow : Installation", () => {
     cy.beforeEachInstallation(); //commands
   });
 
+  // it("Work flow : Installation > โปรแกรม Softether VPN Client > วิธีติดตั้ง", () => {
+  //   cy.get('[class="dropdown-menu"]')
+  //     .children()
+  //     .children()
+  //     .children()
+  //     .children()
+  //     .contains(" วิธีติดตั้ง")
+  //     .should("have.attr", "href", "/pages/install-softether")
+  //     .click({ force: true });
+  // });
+
   it("Work flow : Installation > โปรแกรม Softether VPN Client > วิธีติดตั้ง", () => {
-    cy.get('[class="dropdown-menu"]')
-      .children()
-      .children()
-      .children()
-      .children()
+    cy.get("#w2 > li.dropdown-submenu > ul > li:nth-child(1) > a")
       .contains(" วิธีติดตั้ง")
       .should("have.attr", "href", "/pages/install-softether")
       .click({ force: true });
