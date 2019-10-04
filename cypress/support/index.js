d3399f6efd8652e0f1658e6850c88107f23824e3
thitipat <thitipat.cpe18@hotmail.com>
2019-10-04 17:02:30 +0700

    Change file name, check and update code.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,8 +1,8 @@
-import "./1_mainpage_commands";
-import "./2_register_commands";
-import "./3_login_commands";
-import "./4_price_package_commands";
-import "./5_installation_commands";
-import "./6_instruction_commands";
-import "./7_download_commands";
-import "./8_help_commands";
+import "./1_commands_mainpage";
+import "./2_commands_register";
+import "./3_commands_login";
+import "./4_commands_price_package";
+import "./5_commands_installation";
+import "./6_commands_instruction";
+import "./7_commands_download";
+import "./8_commands_help";
