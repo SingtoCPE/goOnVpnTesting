6dba4cf8b88292c6520b15bb057c0d7485a649aa
thitipat <thitipat.cpe18@hotmail.com>
2019-10-06 16:57:15 +0700

    Check and update code.

--- cypress/integration/goonvpn_work_flow/8_workflow_help.spec.js
+++ cypress/integration/goonvpn_work_flow/8_workflow_help.spec.js
@@ -2,19 +2,21 @@
 
 describe("Work flow : Download", () => {
   beforeEach("Go to Download.", () => {
-    cy.beforeEachHelp(); //commands
+    cy.beforeEachHelp();
   });
 
-  it("Work flow : Download > ตอบ-ถามปัญหาการใช้งาน",  () => {
+  it("Work flow : Download > ตอบ-ถามปัญหาการใช้งาน", () => {
     cy.get('[class="dropdown-menu"]')
-    .contains('ตอบ-ถามปัญหาการใช้งาน')
-    .click()
-    cy.loopFAQ()
+      .contains("ตอบ-ถามปัญหาการใช้งาน")
+      .click();
+    cy.url().should("eq", "https://www.goonvpn.com/support");
+    cy.loopFAQ();
   });
 
-  it("Work flow : Download > ติดต่อเรา",  () => {
+  it("Work flow : Download > ติดต่อเรา", () => {
     cy.get('[class="dropdown-menu"]')
-    .contains('ติดต่อเรา')
-    .click()
+      .contains("ติดต่อเรา")
+      .click();
+    cy.url().should("eq", "https://www.goonvpn.com/#");
   });
-})
+});
